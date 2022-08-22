005498665e0f018c82abc9fdbeb762cd4066fa9a
asuka-iino <ronask.99@gmail.com>
2022-08-22 15:22:15 +0900

    almostly_comp

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,12 +17,15 @@ class TodosController < ApplicationController
 
   # GET /todos/1/edit
   def edit
+    @todo = Todo.find(params[:id])
   end
 
   # POST /todos or /todos.json
   def create
-    @todo = Todo.new(todo_params)
-
+    todo = Todo.new(todo_params)
+    todo.save!
+    redirect_to todos_url, notice: "タスクを登録しました"
+=begin
     respond_to do |format|
       if @todo.save
         format.html { redirect_to todo_url(@todo), notice: "Todo was successfully created." }
@@ -32,11 +35,17 @@ class TodosController < ApplicationController
         format.json { render json: @todo.errors, status: :unprocessable_entity }
       end
     end
+=end
   end
 
   # PATCH/PUT /todos/1 or /todos/1.json
   def update
-    respond_to do |format|
+=begin
+   todo = Todo.find(params[:id])
+    todo.update!(todo_params)
+    redirect_to todo_url, notice: "タスクを更新しました"
+=end
+  respond_to do |format|
       if @todo.update(todo_params)
         format.html { redirect_to todo_url(@todo), notice: "Todo was successfully updated." }
         format.json { render :show, status: :ok, location: @todo }
@@ -65,6 +74,6 @@ class TodosController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def todo_params
-      params.require(:todo).permit(:task, :deadline)
+      params.require(:todo).permit(:task, :deadline, :description)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_22_012911) do
+ActiveRecord::Schema.define(version: 2022_08_22_044803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_07_22_012911) do
     t.date "deadline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
 end
